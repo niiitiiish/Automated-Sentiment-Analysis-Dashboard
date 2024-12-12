d352fcf28f92d78cac3ff702b74507dc3ed68c78
Nitish kumar <144931952+niiitiiish@users.noreply.github.com>
2024-12-12 23:23:28 +0530

    Update senti.py

--- senti.py
+++ senti.py
@@ -1,6 +1,6 @@
 import streamlit as st
-from nltk.sentiment.vader import SentimentIntensityAnalyzer
 import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
 import pandas as pd
 import plotly.express as px
 
